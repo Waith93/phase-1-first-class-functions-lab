759e36fc1aa01768a7f5172b0dc7795bc1a4e741
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-08 15:03:15 +0300

    first class lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+    console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+};
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+    console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+};
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(10));
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(10));
+
+const selectDifferentDrivers = function(drivers, driverSelectorFunction) {
+    return driverSelectorFunction(drivers);
+  };
+  
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
